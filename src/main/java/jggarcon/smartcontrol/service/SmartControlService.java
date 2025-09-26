45f7a632fcf8c648ab96599a3a404a613067dcc0
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-26 14:23:59 -0300

    added service method to toggle light switch

--- src/main/java/jggarcon/smartcontrol/service/SmartControlService.java
+++ src/main/java/jggarcon/smartcontrol/service/SmartControlService.java
@@ -6,7 +6,6 @@ import jggarcon.smartcontrol.model.Fan;
 import jggarcon.smartcontrol.model.Light;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,8 +15,7 @@ public class SmartControlService {
 
     private final Map<String, Appliance> devices = new LinkedHashMap<>();
 
-    @PostConstruct
-    void init() {
+    public SmartControlService() {
         devices.put("light-1", new Light("light-1", "Living Room Light"));
         devices.put("fan-1", new Fan("fan-1", "Ceiling Fan"));
         devices.put("ac-1", new AirConditioner("ac-1", "Bedroom AC"));
@@ -29,6 +27,19 @@ public class SmartControlService {
 
     public Appliance getById(String id) {
         return devices.get(id);
+    }
+
+    public void toggleLight(String id) {
+        Appliance a = devices.get(id);
+        if(a instanceof Light light) {
+            if(light.isOn()) {
+                light.turnOff();
+            } else {
+                light.turnOn();
+            }
+        } else {
+            throw new IllegalArgumentException("Light is not on");
+        }
     }
 }
 
