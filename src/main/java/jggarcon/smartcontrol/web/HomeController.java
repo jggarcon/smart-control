c891e0bab23d6fd91f89f3132eaf19b406267743
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-26 14:44:23 -0300

    added post mapping controllers for light switch and fan speed

--- src/main/java/jggarcon/smartcontrol/web/HomeController.java
+++ src/main/java/jggarcon/smartcontrol/web/HomeController.java
@@ -4,6 +4,9 @@ import jggarcon.smartcontrol.service.SmartControlService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -19,6 +22,18 @@ public class HomeController {
         model.addAttribute("title", "Smart Control");
         model.addAttribute("devices", smartControlService.getAll());
         return "index";
+    }
+
+    @PostMapping("/light/{id}/toggle")
+    public String toggleLight(@PathVariable String id) {
+        smartControlService.toggleLight(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/fan/{id}/speed")
+    public String setFanSpeed(@PathVariable String id, @RequestParam int speed) {
+        smartControlService.setFanSpeed(id, speed);
+        return "redirect:/";
     }
 }
 
