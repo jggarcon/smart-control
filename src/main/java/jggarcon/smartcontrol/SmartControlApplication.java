644ad86f1de44bbce52f778c7a583ed30dee0a31
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-26 13:16:42 -0300

    Initial commit

---
+++ src/main/java/jggarcon/smartcontrol/SmartControlApplication.java
@@ -0,0 +1,13 @@
+package jggarcon.smartcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jggarcon/smartcontrol/SmartControlApplicationTests.java
@@ -0,0 +1,13 @@
+package jggarcon.smartcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
