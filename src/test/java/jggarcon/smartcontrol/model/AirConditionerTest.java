159b76ddec132f066054defc09c3c44570a7adb7
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-26 16:25:51 -0300

    updated air conditioner test after changing the input mode

--- src/test/java/jggarcon/smartcontrol/model/AirConditionerTest.java
+++ src/test/java/jggarcon/smartcontrol/model/AirConditionerTest.java
@@ -1,24 +1,23 @@
 package jggarcon.smartcontrol.model;
 
 
-import jggarcon.smartcontrol.model.AirConditioner.Mode;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AirConditionerTest {
 
     @Test
     void testTurnOnAndOff() {
-        AirConditioner ac = new AirConditioner("ac-1", "Bedroom AC");
+        AirConditioner ac = new AirConditioner("ac-1", "John's Bedroom AC");
 
         assertEquals("Air Conditioner is OFF", ac.stateLabel());
 
         ac.turnOn();
-        assertEquals("Air Conditioner COOL mode", ac.stateLabel());
+        ac.setTemperatureC(22);
+        assertEquals("Air Conditioner set to 22°C", ac.stateLabel());
 
-        ac.setMode(Mode.HEAT);
-        assertEquals("Air Conditioner HEAT mode", ac.stateLabel());
+        ac.setTemperatureC(28);
+        assertEquals("Air Conditioner set to 28°C", ac.stateLabel());
 
         ac.turnOff();
         assertEquals("Air Conditioner is OFF", ac.stateLabel());
