b66b6e482847f15fdbef198a3bf216f5e64e42f7
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-26 13:48:04 -0300

    implemented initial light class

---
+++ src/main/java/jggarcon/smartcontrol/model/Light.java
@@ -0,0 +1,40 @@
+package jggarcon.smartcontrol.model;
+
+public class Light implements Appliance{
+
+    private final String id;
+    private final String name;
+    private boolean isOn;
+
+    public Light(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.isOn = false;
+    }
+
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void turnOn() {
+        isOn = true;
+    }
+
+    @Override
+    public void turnOff() {
+        isOn = false;
+    }
+
+    @Override
+    public String stateLabel() {
+        return isOn ? "Light is ON" : "Light is OFF";
+    }
+}
